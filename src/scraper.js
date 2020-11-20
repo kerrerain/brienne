2621d678304f3ad936a48b4f0ae31a6419a74e16
Henry BONJOUR <h.bonjour@groupeonepoint.com>
2020-11-20 16:32:44 +0100

    Use test instead of match for RegExp
    The test method is enough to find terms.

--- src/scraper.js
+++ src/scraper.js
@@ -2,6 +2,11 @@ const errors = require("./errors");
 const logger = require("./logger");
 const MODULE_NAME = "[scraper]";
 
+const REGEX_W3C = new RegExp("W3C", "mi");
+const REGEX_WCAG = new RegExp("WCAG", "mi");
+const REGEX_A11Y = new RegExp("accessibilité", "mi");
+const REGEX_RGAA = new RegExp("déclaration de conformité", "mi");
+
 async function explorePage(page, url) {
   logger.debug(`${MODULE_NAME} ${url} Starting.`);
 
@@ -38,27 +43,24 @@ async function explorePage(page, url) {
 
   logger.debug(`${MODULE_NAME} ${url} Searching terms.`);
 
-  const [hasTermW3C, hasTermWCAG, hasTermA11Y, hasTermRGAA] = await Promise.all([
-    _hasTerm(page, "W3C"),
-    _hasTerm(page, "WCAG"),
-    _hasTerm(page, "accessibilite"),
-    _hasTerm(page, "declaration de conformite"),
-  ]).catch(errors.commonErrorHandler);
+  const pageContent = await page
+    .content()
+    .catch(errors.commonErrorHandler);
 
-  checklist.hasTermW3C = hasTermW3C;
-  checklist.hasTermWCAG = hasTermWCAG;
-  checklist.hasTermA11Y = hasTermA11Y;
-  checklist.hasTermRGAA = hasTermRGAA;
+  checklist.hasTermW3C = REGEX_W3C.test(pageContent);
+  checklist.hasTermWCAG = REGEX_WCAG.test(pageContent);
+  checklist.hasTermA11Y = REGEX_A11Y.test(pageContent);
+  checklist.hasTermRGAA = REGEX_RGAA.test(pageContent);
 
   // If there isn't any A11Y or RGAA term, there is nothing more to scrap.
-  if (!hasTermA11Y && !hasTermRGAA) {
-    logger.debug(`${MODULE_NAME} ${url} Finished. Not AY11 or RGAA terms found.`);
+  if (!checklist.hasTermA11Y && !checklist.hasTermRGAA) {
+    logger.debug(`${MODULE_NAME} ${url} Finished. Not any AY11 or RGAA term found.`);
     return checklist;
   }
 
   const [clickableA11Y, clickableRGAA] = await Promise.all([
-    _clickableElements(page, "accessibilite"),
-    _clickableElements(page, "declaration de conformite"),
+    _clickableElements(page, REGEX_A11Y),
+    _clickableElements(page, REGEX_RGAA),
   ]);
 
   checklist.hasClickableA11Y = clickableA11Y.length > 0;
@@ -82,26 +84,13 @@ async function explorePage(page, url) {
 }
 
 /**
- * Searches a term on a page.
- * 
- * @param {object} page The puppeteer page.
- * @param {string} term The term to search for.
- */
-async function _hasTerm(page, term) {
-  const content = (await page.content()).replace(/é/g, "e"); // We only need to remove "é".
-  const found = content.match(new RegExp(`\\b(${term})\\b`, "gmi"));
-  return found != null && found.length > 0;
-}
-
-/**
  * Searches for clickable elements with this term on a page.
  * If no element has been found, returns an empty array.
  * 
  * @param {object} page The puppeteer page.
- * @param {string} term The term to search for.
+ * @param {RegExp} regex The regex to use.
  */
-async function _clickableElements(page, term) {
-  const regex = new RegExp(`\\b(${term})\\b`, "gmi");
+async function _clickableElements(page, regex) {
   const elements = await page.$$eval("a", nodes => nodes.map(n => {
     return {
       url: n.href,
@@ -110,8 +99,7 @@ async function _clickableElements(page, term) {
   }));
 
   return elements.filter(n => {
-    const found = n.text.replace(/é/g, "e").match(regex);
-    return found != null && found.length > 0;
+    return regex.test(n.text);
   });
 }
 
